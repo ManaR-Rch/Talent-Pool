ac71aa136c6015591a4003abf2521b113374571c
ManaR-Rch <manarmarchou6@gmail.com>
2025-04-11 10:02:52 +0100

    candidature repository

--- app/Repositories/Contracts/CandidatureRepositoryInterface.php
+++ app/Repositories/Contracts/CandidatureRepositoryInterface.php
@@ -2,18 +2,3 @@
 
 namespace App\Repositories\Contracts;
 
-interface CandidatureRepositoryInterface
-{
-    public function getAll();
-    public function getById($id);
-    public function getByUser($userId);
-    public function getByAnnonce($annonceId);
-    public function getByUserAndAnnonce($userId, $annonceId);
-    public function create(array $data);
-    public function update($id, array $data);
-    public function updateStatus($id, $status);
-    public function delete($id);
-    public function countByUser($userId);
-    public function countByAnnonce($annonceId);
-    public function countByStatut($statut);
-}
