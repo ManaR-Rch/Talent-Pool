eecd41836845e04b23e74d2612f5d80519803ba6
ManaR-Rch <manarmarchou6@gmail.com>
2025-04-11 10:03:50 +0100

    user repo

--- app/Repositories/Contracts/UserRepositoryInterface.php
+++ app/Repositories/Contracts/UserRepositoryInterface.php
@@ -1,14 +1,3 @@
 <?php
 
 namespace App\Repositories\Contracts;
-
-interface UserRepositoryInterface
-{
-    public function getAll();
-    public function getById($id);
-    public function getByRole($role);
-    public function create(array $data);
-    public function update($id, array $data);
-    public function delete($id);
-    public function countByRole($role);
-}
