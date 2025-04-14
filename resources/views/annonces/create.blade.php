c2c25e1cc18fa1645db32240c26b6099a029adef
ManaR-Rch <manarmarchou6@gmail.com>
2025-04-14 10:08:07 +0100

    annonce create

--- resources/views/annonces/create.blade.php
+++ resources/views/annonces/create.blade.php
@@ -6,7 +6,8 @@
 <div class="py-12">
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
         <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-           
+            <div class="p-6 bg-white border-b border-gray-200">
+                <h1 class="text-2xl font-semibold text-gray-900 mb-6">Publier une annonce</h1>
                 
                 <form action="{{ route('annonces.store') }}" method="POST">
                     @csrf
